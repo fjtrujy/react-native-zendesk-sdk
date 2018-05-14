8ca390b47ce47ef39fc7aa9a502a2060c77d47ac
Francisco Javier Trujillo Mata <francisco.trujillo@adddisongloballtd.com>
2018-05-14 12:21:12 +0200

    Add iOS methods

--- ios/RNZendeskSDK/RNZendeskSDK.m
+++ ios/RNZendeskSDK/RNZendeskSDK.m
@@ -7,145 +7,116 @@
 //
 
 #import "RNZendeskSDK.h"
-//#import "FLEXManager.h"
+#import <React/RCTConvert.h>
+
+@import ZendeskSDK;
+@import ZendeskCoreSDK;
 
 @implementation RNZendeskSDK
+
+static NSString * const RNZendeskAppIDKey = @"appID";
+static NSString * const RNZendeskZendeskClientIDKey = @"clientID";
+static NSString * const RNZendeskZendeskURLKey = @"zendeskURL";
+
+static NSString * const RNZendeskZendeskCustomerNameKey = @"customerName";
+static NSString * const RNZendeskZendeskCustomerEmailKey = @"customerEmail";
+static NSString * const RNZendeskZendeskCustomerUniqueIDKey = @"customerUniqueID";
 
 // To export a module named RNZendeskSDK
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(showExplorer)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-//        [[FLEXManager sharedManager] showExplorer];
-    });
+RCT_EXPORT_METHOD(initialize:(NSDictionary *)config) {
+    NSString *appID = [RCTConvert NSString:config[RNZendeskAppIDKey]];
+    NSString *clientID = [RCTConvert NSString:config[RNZendeskZendeskClientIDKey]];
+    NSString *zendeskURL = [RCTConvert NSString:config[RNZendeskZendeskURLKey]];
+    
+    [ZDKZendesk initializeWithAppId:appID clientId:clientID zendeskUrl:zendeskURL];
+    [ZDKSupport initializeWithZendesk:[ZDKZendesk instance]];
 }
 
-//RCT_EXPORT_METHOD(initialize:(NSDictionary *)config){
-//    NSString *appId = [RCTConvert NSString:config[@"appId"]];
-//    NSString *zendeskUrl = [RCTConvert NSString:config[@"zendeskUrl"]];
-//    NSString *clientId = [RCTConvert NSString:config[@"clientId"]];
-//    [[ZDKConfig instance]
-//     initializeWithAppId:appId
-//     zendeskUrl:zendeskUrl
-//     clientId:clientId];
-//}
-//
-//RCT_EXPORT_METHOD(setupIdentity:(NSDictionary *)identity){
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        ZDKAnonymousIdentity *zdIdentity = [ZDKAnonymousIdentity new];
-//        NSString *email = [RCTConvert NSString:identity[@"customerEmail"]];
-//        NSString *name = [RCTConvert NSString:identity[@"customerName"]];
-//        if (email != nil) {
-//            zdIdentity.email = email;
-//        }
-//        if (name != nil) {
-//            zdIdentity.name = name;
-//        }
-//        [ZDKConfig instance].userIdentity = zdIdentity;
-//        
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(showHelpCenterWithOptions:(NSDictionary *)options) {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        ZDKHelpCenterOverviewContentModel *helpCenterContentModel = [ZDKHelpCenterOverviewContentModel defaultContent];
-//        helpCenterContentModel.hideContactSupport = [RCTConvert BOOL:options[@"hideContactSupport"]];
-//        if (helpCenterContentModel.hideContactSupport) {
-//            [ZDKHelpCenter setNavBarConversationsUIType:ZDKNavBarConversationsUITypeNone];
-//        }
-//        vc.modalPresentationStyle = UIModalPresentationFormSheet;
-//        [ZDKHelpCenter presentHelpCenterOverview:vc withContentModel:helpCenterContentModel];
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(showCategoriesWithOptions:(NSArray *)categories options:(NSDictionary *)options) {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        ZDKHelpCenterOverviewContentModel *helpCenterContentModel = [ZDKHelpCenterOverviewContentModel defaultContent];
-//        helpCenterContentModel.groupType = ZDKHelpCenterOverviewGroupTypeCategory;
-//        helpCenterContentModel.groupIds = categories;
-//        helpCenterContentModel.hideContactSupport = [RCTConvert BOOL:options[@"hideContactSupport"]];
-//        if (helpCenterContentModel.hideContactSupport) {
-//            [ZDKHelpCenter setNavBarConversationsUIType:ZDKNavBarConversationsUITypeNone];
-//        }
-//        vc.modalPresentationStyle = UIModalPresentationFormSheet;
-//        [ZDKHelpCenter presentHelpCenterOverview:vc withContentModel:helpCenterContentModel];
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(showSectionsWithOptions:(NSArray *)sections options:(NSDictionary *)options) {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        ZDKHelpCenterOverviewContentModel *helpCenterContentModel = [ZDKHelpCenterOverviewContentModel defaultContent];
-//        helpCenterContentModel.groupType = ZDKHelpCenterOverviewGroupTypeSection;
-//        helpCenterContentModel.groupIds = sections;
-//        helpCenterContentModel.hideContactSupport = [RCTConvert BOOL:options[@"hideContactSupport"]];
-//        if (helpCenterContentModel.hideContactSupport) {
-//            [ZDKHelpCenter setNavBarConversationsUIType:ZDKNavBarConversationsUITypeNone];
-//        }
-//        vc.modalPresentationStyle = UIModalPresentationFormSheet;
-//        [ZDKHelpCenter presentHelpCenterOverview:vc withContentModel:helpCenterContentModel];
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(showLabelsWithOptions:(NSArray *)labels options:(NSDictionary *)options) {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        ZDKHelpCenterOverviewContentModel *helpCenterContentModel = [ZDKHelpCenterOverviewContentModel defaultContent];
-//        helpCenterContentModel.labels = labels;
-//        helpCenterContentModel.hideContactSupport = [RCTConvert BOOL:options[@"hideContactSupport"]];
-//        if (helpCenterContentModel.hideContactSupport) {
-//            [ZDKHelpCenter setNavBarConversationsUIType:ZDKNavBarConversationsUITypeNone];
-//        }
-//        vc.modalPresentationStyle = UIModalPresentationFormSheet;
-//        [ZDKHelpCenter presentHelpCenterOverview:vc withContentModel:helpCenterContentModel];
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(showHelpCenter) {
-//    [self showHelpCenterWithOptions:nil];
-//}
-//
-//RCT_EXPORT_METHOD(showCategories:(NSArray *)categories) {
-//    [self showCategoriesWithOptions:categories options:nil];
-//}
-//
-//RCT_EXPORT_METHOD(showSections:(NSArray *)sections) {
-//    [self showSectionsWithOptions:sections options:nil];
-//}
-//
-//RCT_EXPORT_METHOD(showLabels:(NSArray *)labels) {
-//    [self showLabelsWithOptions:labels options:nil];
-//}
-//
-//RCT_EXPORT_METHOD(callSupport:(NSDictionary *)customFields) {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        
-//        NSMutableArray *fields = [[NSMutableArray alloc] init];
-//        
-//        for (NSString* key in customFields) {
-//            id value = [customFields objectForKey:key];
-//            [fields addObject: [[ZDKCustomField alloc] initWithFieldId:@(key.intValue) andValue:value]];
-//        }
-//        [ZDKConfig instance].customTicketFields = fields;
-//        [ZDKRequests presentRequestCreationWithViewController:vc];
-//    });
-//}
-//
-//RCT_EXPORT_METHOD(supportHistory){
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIWindow *window=[UIApplication sharedApplication].keyWindow;
-//        UIViewController *vc = [window rootViewController];
-//        [ZDKRequests presentRequestListWithViewController:vc];
-//    });
-//}
+
+RCT_EXPORT_METHOD(setIdentity:(NSDictionary *)identity) {
+    NSString *name = [RCTConvert NSString:identity[RNZendeskZendeskCustomerNameKey]];
+    NSString *email = [RCTConvert NSString:identity[RNZendeskZendeskCustomerEmailKey]];
+    
+    ZDKObjCAnonymous *userIdentity = [[ZDKObjCAnonymous alloc] initWithName:name email:email];
+    [[ZDKZendesk instance] setIdentity:userIdentity];
+}
+
+RCT_EXPORT_METHOD(setUniqueIdentity:(NSDictionary *)identity) {
+    NSString *token = [RCTConvert NSString:identity[RNZendeskZendeskCustomerUniqueIDKey]];
+    
+    ZDKObjCJwt *uniqueUserIdentity = [[ZDKObjCJwt alloc] initWithToken:token];
+    [[ZDKZendesk instance] setIdentity:uniqueUserIdentity];
+}
+
+RCT_EXPORT_METHOD(presentHelpCenterOverview) {
+    [self presentHelpCenterOverviewWithConfiguration:nil];
+}
+
+RCT_EXPORT_METHOD(presentHelpCenterOverviewWithConfiguration:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *helpCenter = [ZDKHelpCenterUi buildHelpCenterOverviewWithConfigs:configurations];
+    [self presentZendeskViewController:helpCenter];
+}
+
+RCT_EXPORT_METHOD(presentbuildHelpCenterWithArticle:(ZDKHelpCenterArticle *)article) {
+    [self presentbuildHelpCenterWithArticle:article configs:nil];
+}
+
+RCT_EXPORT_METHOD(presentbuildHelpCenterWithArticle:(ZDKHelpCenterArticle *)article configs:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *helpCenter = [ZDKHelpCenterUi buildHelpCenterArticle:article andConfigs:configurations];
+    [self presentZendeskViewController:helpCenter];
+}
+
+RCT_EXPORT_METHOD(presentbuildHelpCenterWithArticleId:(NSString *)articleId) {
+    [self presentbuildHelpCenterWithArticleId:articleId configs:nil];
+}
+
+RCT_EXPORT_METHOD(presentbuildHelpCenterWithArticleId:(NSString *)articleId configs:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *helpCenter = [ZDKHelpCenterUi buildHelpCenterArticleWithArticleId:articleId andConfigs:configurations];
+    [self presentZendeskViewController:helpCenter];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestList) {
+    [self presentbuildRequestListWithConfiguration:nil];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestListWithConfiguration:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *requestList = [ZDKRequestUi buildRequestListWith:configurations];
+    [self presentZendeskViewController:requestList];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestUI) {
+    [self presentbuildRequestUIWithConfiguration:nil];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestUIWithConfiguration:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *requestUI = [ZDKRequestUi buildRequestUiWith:configurations];
+    [self presentZendeskViewController:requestUI];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestUIWithRequestId:(NSString *)requestId) {
+    [self presentbuildRequestUIWithRequestId:requestId configs:nil];
+}
+
+RCT_EXPORT_METHOD(presentbuildRequestUIWithRequestId:(NSString *)requestId configs:(NSArray<ZDKUiConfiguration> *)configurations) {
+    UIViewController *requestUI = [ZDKRequestUi buildRequestUiWithRequestId:requestId configurations:configurations];
+    [self presentZendeskViewController:requestUI];
+}
+
+#pragma mark - Helper Methods
+
+- (UIViewController *)rootViewController {
+    UIWindow *window=[UIApplication sharedApplication].keyWindow;
+    return [window rootViewController];
+}
+
+- (void)presentZendeskViewController:(UIViewController *)viewController {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *rootVC = [self rootViewController];
+        UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:viewController];
+        [rootVC presentViewController:navController animated:YES completion:nil];
+    });
+}
 
 @end
