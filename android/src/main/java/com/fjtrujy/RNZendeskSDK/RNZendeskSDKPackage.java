c4f02cf48af1dc8f09ef39bcc4463e89757ea3b6
Francisco Javier Trujillo Mata <francisco.trujillo@adddisongloballtd.com>
2018-05-15 16:49:03 +0200

    Add Android methods

---
+++ android/src/main/java/com/fjtrujy/RNZendeskSDK/RNZendeskSDKPackage.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Francisco Javier Trujillo Mata on 5/16/18.
+ * https://github.com/fjtrujy/react-native-zendesk-sdk
+ */
+
+package com.fjtrujy.flex;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+public class RNFlipboardFlexPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      return Arrays.<NativeModule>asList(new RNFlipboardFlex(reactContext));
+    }
+
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+      return Collections.emptyList();
+    }
+}
